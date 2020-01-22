f0ad15b15e3a78b72f4c7d3878b1d08de2e99653
Brendan <brendan.metzger@gmail.com>
2020-01-22 10:17:56 -0600

    adding better re

--- markdom.php
+++ markdom.php
@@ -2,7 +2,7 @@
 
 // NEXT: parse links
 
-/****        ************************************************************************** MarkDom */
+/****         ************************************************************************** MarkDom */
 class MarkDOM {
   private $doc = null;
   
@@ -11,12 +11,12 @@ class MarkDOM {
     $this->doc->formatOutput = true;
     $this->doc->loadXML("<{$root}/>");
     $prior = $this->doc->documentElement;
-    foreach ($this->parse($text) as $block) {
+    foreach ($this->split($text) as $block) {
       $prior = $block->render($prior);
     }
   }
   
-  private function parse(string $text) {
+  private function split(string $text) {
     // replace pilcrows and various line breaks with \n, replace tabs with spaces
     $filtered = preg_replace(['/¶|\r\n|\r/u', '/\t/'], ["\n",'    '], $text); 
     return array_map('Block::Lexer', array_filter(explode("\n", $filtered), 'MarkDOM::notEmpty'));
@@ -34,8 +34,7 @@ class MarkDOM {
 /****       ****************************************************************************** LEXER */
 class Block {
   public $name, $text, $depth, $symbol;
-  // TODO consider an atomic capture that will exit
-  static public $elements = [
+  static public $tags = [
     'li'         => '[0-9]+\.|- ',
     'h'          => '#{1,6}',
     'pre'        => '`{4}',
@@ -44,23 +43,23 @@ class Block {
     'p'          => '[a-z]',
   ];
 
-  static public function Lexer(string $line) {
-    //7.4  implode('|', array_map(fn($re):string => "({$re})", self::$elements);
+  static public function Lexer(string $text) {
+    //7.4  implode('|', array_map(fn($re):string => "({$re})", self::$tags);
     $exp = implode('|', array_map(function($regex) { 
       return "({$regex})";
-    }, self::$elements));
+    }, self::$tags));
     
-    $key = preg_match("/\s*{$exp}/Ai", $line, $list, PREG_OFFSET_CAPTURE) ? count($list) - 2 : 0;
+    $key = preg_match("/\s*{$exp}/Ai", $text, $list, PREG_OFFSET_CAPTURE) ? count($list) - 2 : 0;
     
     $match = array_pop($list) ?? [null, 0];
-    $type = array_keys(self::$elements)[$key];
-    return new $type(new self($type, $line, ...$match));
+    $type = array_keys(self::$tags)[$key];
+    return new $type(new self($type, $text, ...$match));
   }
 
-  public function __construct(string $name, string $line, string $symbol, int $indent) {
+  public function __construct(string $name, string $text, string $symbol, int $indent) {
     $offset = strlen($symbol);
     $this->name   = $name != 'h' ?  $name :  "h" . $offset;
-    $this->text   = trim(substr($line, $indent + (($name == 'p') ? 0 : $offset)));
+    $this->text   = trim(substr($text, $indent + (($name == 'p') ? 0 : $offset)));
     $this->depth  = floor($indent/2);
     $this->symbol = $symbol;
   } 
@@ -72,22 +71,24 @@ class Inline {
   private $block;
   //(?:(\_+|\^|~{2}|`+|=)([^*^~`=]+)\1)
   private static $symbols = null;
-  public $elements = [
-    'Anchor' => '(!?)\[([^\)^\[]+)\]\(([^\)]+)(?:\"([^"]+)\")?\)',
-    'Strong' => '[_*]{2}',
-    'Italic' => '[_*]',
-    'Mark'   => '=',
-    'Quote'  => '"',
-    'Code'   => '`',
-    'Samp'   => '`{2}',
-    'Strike' => '~{2}',
-    'Abbrev' => '\^{2}',
+  public $tags = [
+    'a'      => '(!?)\[([^\)^\[]+)\]\(([^\)]+)(?:\"([^"]+)\")?\)',
+    'strong' => '[_*]{2}',
+    'em'     => '[_*]',
+    'mark'   => '=',
+    'q'      => '"',
+    'code'   => '`',
+    'samp'   => '`{2}',
+    's'      => '~{2}',
+    'abbr'   => '\^{2}',
+    'time'   => '\+',
+    'input'  => '\[[ox]\]'
   ];
-  // add sup/sub
+  // add sup/sub, time
   public function __construct($block) {
     $this->block = $block;
-    // php 7.4 allows self::$symbols ??= preg_replace(...)
-    self::$symbols = self::$symbols ?? preg_replace("/[\d{}+?:]/", '', count_chars(implode('', $this->elements), 3));
+    //7.4 self::$symbols ??= preg_replace("/[\d{}+?:]/", '', count_chars(implode('', $this->tags), 3));
+    self::$symbols = self::$symbols ?? preg_replace("/[\d{}+?:]/", '', count_chars(implode('', $this->tags), 3));
   }
   
   public function inject($elem) {
@@ -98,7 +99,7 @@ class Inline {
   
   public function parse($text) {
     
-    foreach ($this->elements as $name => $re) {
+    foreach ($this->tags as $name => $re) {
       // print_r($re);
       // while(preg_match("/{$re}/u", $text, $match, PREG_OFFSET_CAPTURE)) {
       //   // continue/break or reset loop after replacing text
@@ -123,9 +124,8 @@ class p {
     $this->value  = new Inline($this);
   }
 
-  public function render($previous = null) {
-
-    if ($previous->context === null) 
+  public function render($previous) {
+    if ($previous instanceof DOMElement) 
       $this->context =  $previous;
     else if ($this->lexer->name != $previous->lexer->name && $previous->reset)
       $this->context = $previous->reset;
@@ -154,7 +154,7 @@ class li extends p {
   }
   
   // TODO: there has to be recursion somewhere in here. This hurts the eyeballs right now
-  public function render($previous = null) {
+  public function render($previous) {
     $this->reset = $previous->reset;
     $depth       = $this->lexer->depth;
     if ($previous->lexer->name != 'li') {
