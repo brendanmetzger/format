99a8e4a013189df4ff5eb945bf4103338a76dfa6
Brendan <brendan.metzger@gmail.com>
2020-02-08 10:17:04 -0600

    working out evaluation

--- markdom.php
+++ markdom.php
@@ -13,10 +13,10 @@
 class MarkDOM {
   public function __construct($path, $root = 'article') {
     $this->doc = new DOMDocument('1.0', 'UTF-8');
-    echo "\n\n\n\n\n\n\n\n\n\n\n\n\n\n RUNNING AGAIN \n\n\n\n";
+    $this->doc->loadXML("<{$root}/>");
     foreach (Tokenizer::scan($path, $this->doc) as $block) {
       $rendered = $block->render();
-      print_r($rendered);
+      // print_r($rendered);
     }
   }
 
@@ -27,99 +27,138 @@ class MarkDOM {
 
 class Tokenizer {
   const BLOCK = [
-    'keys' => [ 'ol'   , 'ul' ,    'h'  ,  'pre' , 'blockquote',  'hr'  , 'comment',  'p'  ],
-    'rgxp' => ['\d+\.' , '- ' , '#{1,6}', '`{3}' ,     '>'     , '-{3,}',  '\/\/'  , '\S'  ],
-    'exit' => [ false  , false,  false  ,  true  ,    false    ,  false ,  false   , false ],
-    'node' => [ 'li'   , 'li' ,   '*'   ,  false ,     'p'     ,  null  ,  false   ,  '*'  ],
+  //'rgxp' => '/\s*(?:(\d+\.)|(- )|(#{1,6})|(`{3})|(>)|(-{3,})|(\/\/)|(\S))/Ai',
+    'name' => [ 'ol'   , 'ul' ,    'h'  ,  'pre' , 'blockquote',  'hr'  , 'comment',  'p'   ],
+    'rgxp' => ['\d+\.' , '- ' , '#{1,6}', '`{3}' ,     '>'     , '-{3,}',  '\/\/'  , '\S'   ],
+    'join' => [ false  , false,  false  ,  true  ,    false    ,  false ,  false   , false  ],
+    'tots' => [ 'li'   , 'li' , 'PCDATA', 'CDATA',     'p'     , 'EMPTY', 'CDATA'  ,'PCDATA'],
   ];
   
   // these are really better suited to a concept of 'fences', and pre would be involved in one. lots of pondering still
-  const INLINE = [
-    'q'      => '"([^"]+)"',
+  const BOUND = [
+    'q'      => '"([^"]+)"',        // " 
+    'strong' => '\*\*([^*]+)\*\*',  // **
+    'em'     => '\*([^\*]+)\*',     // *
+    'mark'   => '\|([^|]+)\|',      // |
+    'time'   => '``([^``]+)``',     // ``
+    'code'   => '`([^`]+)`',        // `
+    's'      => '~~([^~~]+)~~',     // ~~
+    'abbr'   => '\^\^([^\^]+)\^\^', // ^^
+  ];
+  
+  // links, inputs, img (perhaps more; sup, sub, audio video figure, table, footnotes, etc) st|nd|rd|th|[0-9.]
+  const HYBRID = [
+    // TODO, right now this does img too.. would rather something <whatever.jpg> do the trick, as a general embedder
     'a'      => '(!?)\[([^\)^\[]+)\]\(([^\)]+)(?:\"([^"]+)\")?\)',
     'input'  => '^\[([x\s])\](.*)$',
-    // TODO, right now this does img too.. would rather something <whatever.jpg> do the trick, as a general embedder
-    'strong' => '\*\*([^*]+)\*\*',
-    'em'     => '\*([^\*]+)\*',
-    'mark'   => '\|([^|]+)\|',
-    'time'   => '``([^``]+)``',
-    'code'   => '`([^`]+)`',
-    's'      => '~~([^~~]+)~~',
-    'abbr'   => '\^\^([^\^]+)\^\^',
   ];
   
   static public function blockmatch($text) {
-    //7.4   sprintf("/%s/Ai",implode('|', array_map(fn($re) => "({$re})", self::BLOCK['rgxp']));
-    $rgxp = sprintf("/%s/Ai", implode('|', array_map(function($regex) { 
-      return "\s*({$regex})";
+    //7.4   sprintf("/%s/Ai", implode('|', array_map(fn($re) => "({$re})", self::BLOCK['rgxp']));
+    $rgxp = sprintf("/\s*(?:%s)/Ai", implode('|', array_map(function($re) { 
+      return "({$re})";
     }, self::BLOCK['rgxp'])));
-    
-    if (preg_match($rgxp, $text, $list, PREG_OFFSET_CAPTURE) < 1) return false;
 
-    $match = array_pop($list); // last match contains match & offset: [string $symbol, int offset]
-    $index = count($list)-1;
-    return [
-      'key'    => self::BLOCK['keys'][$index],
-      'exit'   => self::BLOCK['exit'][$index],
-      'symbol' => $match[0],
-      'depth'  => floor($match[1] / 2),
-    ];
+    if (preg_match($rgxp, $text, $list, 0b100000000) < 1) return false;
+
+    $match  = array_pop($list); // last match contains match & offset: [string $symbol, int offset]
+    $column = array_combine(array_keys(self::BLOCK), array_column(self::BLOCK, count($list) - 1));
+
+    return array_merge([
+      'mark'  => $match[0],
+      'depth' => floor($match[1] / 2),
+    ], $column);
   }
   
-  static public function scan($path, DOMDocument $context) {    
-    try {
-      $handle = fopen($path, 'r');
-      $block = new Block($context);
+  public function boundmatch() {
+    $chars = count_chars(implode('', self::BOUND), 3);
+  }
+  
+  static public function scan($path, DOMDocument $context) {
+    $block = new Block($context);
 
-      while ($line = fgets($handle)) {
-        $block->capture($line);
-        if (! $block->state(2)) continue;
-        yield $block;
-        $block = new Block($context);
-      }
-    } finally {
-      fclose($handle);
+    foreach (new SplFileObject($path) as $line) {
+
+      $block->capture($line);
+      if ($block->state(Block::FINISHED) === false) continue;
+      yield $block;
+      $block = new Block($context);
     }
   }
 }
 
-/****       ****************************************************************************** LEXER */
+/****       ****************************************************************************** BLOCK */
 class Block {
+  const READY = 0; const SCANNING = 1; const FINISHED = 2;
   
   public $doc, $context = null;
-  // Status:  0=ready, 1=parsing and 2=finish
-  private $status = 0, $lines = [], $parsed = [], $exit = '';
-  
-  
-  
+  private $status = 0, $lexeme = [], $munch = null, $exit = '';
+    
   public function __construct(DOMDocument $doc) {
-    // $this->doc = $doc;
+    $this->doc = $doc;
   }
   
   public function state(int $status): bool {
     return $status === $this->status;
   }
-  
+
   public function capture(string $text) {
-    if ($this->status === 0) {
-      if ($match = Tokenizer::blockmatch($text)) {
-        $this->parsed[] = $match;
-        $this->status  += 1;
-        // check for if the capture exits on a condition (fenced)
-        if ($match['exit']) {
-          $this->exit = $match['symbol'];
+    if ($this->status === self::READY) {
+      if ($this->munch = Tokenizer::blockmatch($text)) {
+
+        $this->status = self::SCANNING;
+
+        if ($this->munch['join']) {
+          $this->exit = $this->munch['mark'];
           return;
         }
+
       } else return;
-      
-    } else if (!empty($this->parsed) && rtrim($text) == $this->exit) {
-      return $this->status += 1;
+
+    } else if ($this->munch && rtrim($text) === $this->exit) {
+      return $this->status = self::FINISHED;
     }
-    $this->lines[] = $text;
+    $this->lexeme[] = $text;
   }
   
   public function render() {
+    // START: create a generator function and load with current config ($munch);
+    $context = $this->doc->documentElement;
+
+    foreach($this->process($this->munch) as $element) {
+      $context->appendChild($element);
+    }
     return $this;
+  }
+  
+  private function evaluate($lexeme, $munch) {
+
+    if (empty($munch)) {
+      return;
+      // return $munch->appendData($lexeme);
+      
+    }
+    
+    if ($munch['join'] && $munch['tots'] === 'CDATA') {
+      return $this->doc->createCDATASection($lexeme);
+    }
+    
+    if ($munch['name'] === 'comment') {
+      return $this->doc->createComment($lexeme);
+    }
+    
+    // now we do shitloads of work with type/depth 
+    return $this->doc->createElement($munch['name']);
+    
+    // $gen->send();
+  }
+  
+  private function process($munch) {
+    foreach($this->lexeme as $lexeme) { 
+      $node = $this->evaluate($lexeme, $munch || Tokenizer::blockmatch($lexeme));
+      yield $node;
+      $munch = $node instanceof DOMCharacterData ? $node : false;
+    }
   }
   
 }
@@ -132,15 +171,10 @@ class Inline {
   
   public function __construct($text, DOMDocument $doc) {
     $this->node = $doc->createDocumentFragment();
-    $this->frag = $doc->createDocumentFragment();
-    $this->frag->textContent = $text;
     $this->doc  = $doc;
     $this->text = $text;
   }
   
-  public function getFlags() {
-    count_chars(implode('', INLINE::tags), 3);
-  }
     
   public function inject($elem) {
     if (empty($this->text)) return;
@@ -154,7 +188,7 @@ class Inline {
     // element nodes, so the constant saving and outputing and injecting/appending of xml isn't done unnecessarily
     foreach (INLINE::tags as $name => $re) {
       
-      preg_match_all("/{$re}/u", $this->frag->textContent, $hits, PREG_OFFSET_CAPTURE);
+      preg_match_all("/{$re}/u", $this->frag->textContent, $hits, 0b100000000);
       foreach (array_reverse($hits[0]) as [$k, $i]) {
         // $N = $var->firstChild->splitText(mb_strlen(substr($var, 0, $i), 'UTF-8'))
         //                      ->splitText(strlen($k))->previousSibling;
@@ -166,7 +200,7 @@ class Inline {
   
   public function parse($text) {
     foreach (INLINE::tags as $name => $re) {
-      if (preg_match_all("/{$re}/u", $text, $hits, PREG_SET_ORDER) > 0)
+      if (preg_match_all("/{$re}/u", $text, $hits, 0b10) > 0)
         foreach ($hits as $hit) $text = str_replace($hit[0], self::{$name}($this->doc, ...$hit), $text);
     }
     return $text;
